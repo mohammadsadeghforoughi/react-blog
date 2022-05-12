2428c53c0e8aec14027bfde4fd4ee32d7928e59f
Mohammad Foroughi <mohammadforoughi@Mohammads-MacBook-Pro.local>
2022-05-12 20:09:22 +0430

    add subscribe to newsletter component

--- src/components/NewsletterSubscribe/NewsletterSubscribe.style.ts
+++ src/components/NewsletterSubscribe/NewsletterSubscribe.style.ts
@@ -4,20 +4,41 @@ import { makeStyles, createStyles } from "@mui/styles";
 export default makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: "320px !important",
+      width: "100% !important",
+      height: "20rem !important",
       justifyContent: "space-between",
       alignItems: "center",
+      boxShadow:
+        theme.palette.mode == "dark"
+          ? "0 2px 15px 0 rgba(26,26,27,0.637) !important "
+          : "0 2px 15px 0 rgba(210,214,220,0.5) !important",
     },
-    avatar: {
-      width: "3.5rem !important",
-      height: "3.5rem !important",
+    gridContainer: {
+      height: "100% !important",
     },
-    authorBio: {
-      maxWidth: "250px",
-      fontFamily: "Merriweather !important ",
+    leftBox: {
+      backgroundColor: theme.palette.mode == "dark" ? "#363C48" : "#F9FAFB",
+      height: "100% !important",
+      padding: "40px",
     },
-    authorBioLink: {
-      color: theme.palette.primary.main,
+    rightBox: {
+      backgroundColor: theme.palette.mode == "dark" ? "#272C35" : "#fff",
+      height: "100% !important",
+      padding: "40px",
+    },
+    inputs: {
+      width: "100%",
+    },
+    submitButton: {
+      fontSize: "14px !important",
+      height: "3rem !important",
+      width: "8rem !important",
+      borderRadius: "22px !important",
+    },
+    smallText: {
+      width: "80% !important",
+      fontSize: "13px !important",
+      fontWeight: "300 !important",
     },
   })
 );
--- src/components/NewsletterSubscribe/index.tsx
+++ src/components/NewsletterSubscribe/index.tsx
@@ -1,8 +1,84 @@
-import { Card, Typography } from "@mui/material";
+import {
+  Card,
+  Grid,
+  Typography,
+  Box,
+  Avatar,
+  TextField,
+  Button,
+} from "@mui/material";
 import React from "react";
 import useStyles from "./NewsletterSubscribe.style";
+import Mail from "../../assets/mail.svg";
 export const NewsletterSubscribe: React.FC = () => {
   const classes = useStyles();
 
-  return <Card className={classes.root}>dasd</Card>;
+  return (
+    <Card className={classes.root}>
+      <Grid container className={classes.gridContainer}>
+        <Grid xs={6}>
+          <Box className={classes.leftBox}>
+            <Grid container flexDirection={"column"} spacing={3}>
+              <Grid item>
+                <Typography fontWeight={900} fontSize={"20px"}>
+                  Subscribe to the Newsletter
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography fontWeight={400} fontSize={"17px"}>
+                  Subscribe to get my latest content by email.
+                </Typography>
+              </Grid>
+              <Grid item>
+                <img src={Mail} />
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid xs={6}>
+          {" "}
+          <Box className={classes.rightBox}>
+            <Grid container flexDirection={"column"} spacing={2}>
+              <Grid item>
+                <TextField
+                  className={classes.inputs}
+                  hiddenLabel
+                  id="filled-hidden-label-small"
+                  variant="outlined"
+                  size="small"
+                  placeholder="Your first name"
+                  type={"text"}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  className={classes.inputs}
+                  hiddenLabel
+                  id="filled-hidden-label-small"
+                  variant="outlined"
+                  size="small"
+                  placeholder="Your email address"
+                  type={"email"}
+                />
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.submitButton}
+                >
+                  Subscribe
+                </Button>
+              </Grid>
+              <Grid item>
+                <Typography color="secondary" className={classes.smallText}>
+                  I won’t send you spam. Unsubscribe at any time.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+      </Grid>
+    </Card>
+  );
 };
